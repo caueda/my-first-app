cca6d2d0930a86a070655025f5988cf8524b1d92
Carlos R. Ueda <carlos.ueda@gmail.com>
2019-07-15 15:51:44 -0400

    First assignment.
    Build an warning-alert and success-alert components.

---
+++ src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-warning-alert',
+    template: `<div class="alert alert-danger">Warning Alert</div>`
+})
+export class WarningAlertComponent {
+
+}
