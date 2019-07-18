92a8e756b8e3a9e1e3b7fe5d29faaa6397ed0137
Carlos Ueda <carlos.ueda@gmail.com>
2019-07-18 05:35:10 -0300

    Implementing the application pages

--- src/app/warning-alert/warning-alert.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'app-warning-alert',
-    template: `<div class="alert alert-danger">Warning Alert</div>`
-})
-export class WarningAlertComponent {
-
-}
