8d42f4639119c0c77626d130afbc6f1e3289592d
Carlos Ueda <carlos.ueda@gmail.com>
2020-08-20 05:40:24 -0400

    Updating angular and bootstrap. Closes #9
    Angular 8 to 10 and bootstrap 3 to 4.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -5,7 +5,7 @@ import { EventEmitter, Injectable } from '@angular/core';
 export class RecipeService {
     recipes: Recipe[] = [
         new Recipe('A Test1 Recipe', 'This is simply a test1', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
-        new Recipe('A Test2 Recipe', 'This is simply a test2', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg')
+        new Recipe('A Test2 Recipe', 'This is simply a test2', 'https://cdn.pixabay.com/photo/2020/01/28/13/18/noodles-4799804_960_720.jpg')
       ];
 
       recipeSelected = new EventEmitter<Recipe>();
