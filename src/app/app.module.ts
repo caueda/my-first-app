156da4b814c64860787a9c3d75094c1336aa970a
Carlos Ueda <carlos.ueda@gmail.com>
2020-08-20 07:05:45 -0400

    Directive using Renderer2 implemented. Closes #12

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { RecipeDetailComponent } from './recipes/recipe-list/recipe-detail/recip
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { AppBetterHighlightDirective } from './directives/app-better-highlight.directive';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-it
     ShoppingEditComponent,
     RecipeItemComponent,
     DropdownDirective,
-    AppHighlightDirective
+    AppHighlightDirective,
+    AppBetterHighlightDirective
   ],
   imports: [
     BrowserModule
