286eb1c7646039b4168ef3d0c43fbf5176262370
carlos.ueda <carlos.ueda@gmail.com>
2020-09-17 05:59:56 -0400

    Closes #18
    Practice with unsubscribing from an Observable done.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,14 +7,8 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  @Output() pageHeaderEvent = new EventEmitter<string>();
-
   constructor() { }
 
   ngOnInit() {
-  }
-
-  onClick(page: string) {
-    this.pageHeaderEvent.emit(page);
   }
 }
