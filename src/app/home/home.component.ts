99f65357ae7aba1f99ad90bfcaf13d4d50776b20
Carlos Ueda <carlos.ueda@gmail.com>
2020-09-18 05:44:35 -0400

    6-implement-a-pipe-in-the-custominterval. Closes #20

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { interval, Subscription } from 'rxjs';
+import { interval, Observable, Subscription } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -13,9 +14,31 @@ export class HomeComponent implements OnInit, OnDestroy {
   constructor() { }
 
   ngOnInit(): void {
-    this.subscription = interval(1000).subscribe((next) => {
-      console.log(next);
+    // this.subscription = interval(1000).subscribe((next) => {
+    //   console.log(next);
+    // });
+    const customObservalInterval = new Observable((observer) => {
+      let count = 0;
+      setInterval(() =>{
+        observer.next(count);
+        ++count;
+        // if(count === 2) {
+        //   observer.error(new Error("Count === 2"));
+        // }
+        if(count === 3) {
+          observer.complete();
+        }
+
+
+      }, 1000);
     });
+    this.subscription = customObservalInterval.pipe(
+      map((data: number) => {
+        return 'Round ' + (data + 1);
+      })
+    ).subscribe((count) => {
+      console.log(count);
+    }, (error) => console.log(error.message), () => console.log('Completed'));
   }
 
   ngOnDestroy() {
