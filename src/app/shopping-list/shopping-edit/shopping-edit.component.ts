5552cee1ee54c9d528973bcf3e6916f6847f5ef2
carlos.ueda <carlos.ueda@gmail.com>
2020-10-02 20:49:06 -0400

    Shopping-edit. Closes #24
    Implementing the other actions: update, delete and clear using Template Driven Approach.

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,27 +1,50 @@
 import { ShoppingListService } from './../shopping-list.service';
-import { element } from 'protractor';
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 import { FormControl, NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
-export class ShoppingEditComponent implements OnInit {
-
-  constructor(private shoppingListService: ShoppingListService) { }
-
-  form: FormControl;
+export class ShoppingEditComponent implements OnInit, OnDestroy {
+  @ViewChild('f', {static: true}) form: NgForm;
+  subscription: Subscription;
   ingredient: Ingredient = new Ingredient();
+  editMode = false;
+
+  constructor(private shoppingListService: ShoppingListService,) { }
 
   ngOnInit() {
+    this.subscription =
+      this.shoppingListService.ingredientSelected.subscribe((ingredient) => {
+        this.editMode = true;
+        this.ingredient = ingredient;
+      });
   }
 
-  addIngredient(form: NgForm) {
-    console.log('button ingredient pressed');
-    console.log(form);
-    this.shoppingListService.addIngredient(this.ingredient);
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  onSubmit() {
+    if(this.editMode) {
+      this.editMode = false;
+    } else {
+      this.shoppingListService.addIngredient(this.ingredient);
+    }
+    this.ingredient = new Ingredient();
+  }
+
+  onClear() {
+    this.ingredient = new Ingredient();
+    this.editMode = false;
+  }
+
+  onDelete(id: number) {
+    this.shoppingListService.deleteIngredient(id);
+    this.ingredient = new Ingredient();
   }
 }
