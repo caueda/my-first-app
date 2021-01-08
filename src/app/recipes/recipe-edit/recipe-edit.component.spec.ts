6012c14da5f3c64e093ab4b0b351ce8deb06d458
carlos.ueda <carlos.ueda@gmail.com>
2021-01-08 15:35:42 -0400

    Tests fixed. Closes #35

--- src/app/recipes/recipe-edit/recipe-edit.component.spec.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.spec.ts
@@ -1,6 +1,11 @@
+import { ShoppingListService } from './../../shopping-list/shopping-list.service';
+import { RecipeService } from './../recipe.service';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { RecipeEditComponent } from './recipe-edit.component';
+import { AuthService } from 'src/app/auth/auth.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('RecipeEditComponent', () => {
   let component: RecipeEditComponent;
@@ -8,7 +13,9 @@ describe('RecipeEditComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ RecipeEditComponent ]
+      declarations: [ RecipeEditComponent ],
+      imports: [ RouterTestingModule, HttpClientModule ],
+      providers: [ RecipeService, ShoppingListService, AuthService ]
     })
     .compileComponents();
   }));
